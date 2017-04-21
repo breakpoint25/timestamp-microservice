bd48227cb783188104629af2a3c67fbda66f40de
breakpoint25 <breakpoint@gmail.com>
2017-04-20 18:10:23 -0700

    third commit

--- server.js
+++ server.js
@@ -1,11 +1,43 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(bodyParser.raw());
+
+app.get('/:dateVal', (req, res) => {
+  // console.log(req);
+  const dateVal = req.params.dateVal;
+  let date;
+  let unixDate;
+  let nuturalDate;
+
+  if (Number(dateVal) / 1 || Number(dateVal) === 0) {
+    unixDate = dateVal;
+    date = new Date(dateVal * 1000);
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    options.timeZone = 'UTC';
+    nuturalDate = date.toLocaleDateString('en-US', options);
+
+    if (nuturalDate === 'Invalid Date') {
+      nuturalDate = 'null';
+      unixDate = 'null';
+    }
+  } else {
+    nuturalDate = dateVal;
+    unixDate = Math.floor(Date.parse(nuturalDate.concat(' 00:00:00 UTC')) / 1000);
+
+    if (isNaN(unixDate)) {
+      nuturalDate = 'null';
+      unixDate = 'null';
+    } else {
+      unixDate = unixDate.toString();
+    }
+  }
+
+  res.json({ unix: unixDate, natural: nuturalDate });
 });
 
 app.listen(3000, () => {
-  console.log('Example app listening on port 3000!');
+  console.log('App listening on port 3000!');
 });
